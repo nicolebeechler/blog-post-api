39a8985367b86bd6fef7720392a030936e42a4ef
Nicole Beechler <nmbeechler@gmail.com>
2024-01-24 22:47:15 -0500

    Connected server and app to user

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require('dotenv').config()
+const app = require('./app')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => console.log('Mongo is showing love'))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`We in the building ${PORT}`)
 })
